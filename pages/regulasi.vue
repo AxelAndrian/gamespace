5ffb0f58349604131807371e86f661adb54ffe31
Axel <axelandrian001@gmail.com>
2020-05-13 22:26:29 +0700

    Update | Sidebar Improvement, New Page

---
+++ pages/regulasi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Content title="REGULASI">
+      <template #content>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione molestias similique architecto laborum excepturi amet soluta aut ipsa doloribus reprehenderit asperiores sunt aspernatur doloremque totam facere, quae distinctio deleniti ullam!</p>
+      </template>
+    </Content>
+  </div>
+</template>
+
+<script>
+import Content from '@/components/Content.vue'
+
+export default {
+  components: {
+    Content
+  }
+}
+</script>
